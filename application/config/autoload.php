a0acce8bb31c5845b5c462341db48078380ab136
adriitakumi <nicolettekagamine@gmail.com>
2017-09-06 15:37:00 +0800

    csvreader lib

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'form_validation', 'session', 'sla_session');
+$autoload['libraries'] = array('database', 'form_validation', 'session', 'sla_session', 'csv_reader');
 
 /*
 | -------------------------------------------------------------------
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('users_model');
+$autoload['model'] = array('users_model', 'upload_model');
